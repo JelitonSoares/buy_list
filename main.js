1360ee02f44e653ef2a3f9535e74331f1709983c
jeliton soares <jelitonn.soarres@gmail.com>
2023-05-26 12:43:07 -0300

    Adicionando a função de remover os itens das listas

--- main.js
+++ main.js
@@ -8,9 +8,15 @@ let bebida = [];
 let item;
 let categoria;
 let continuar = true;
+let categoriaRemover;
+let itemRemover;
+let indexItem;
+let podeRemover = false;
+
+
 //         Menu Principal
 while(continuar === true) {
-    let menu = prompt('Se Deseja adicionar uma item digite "s", caso não queira digite "n": ');
+    let menu = prompt('Se Deseja adicionar uma item digite "s", caso não queira digite "n" e caso queira remover digite "r": ');
     switch(menu){
         case 's':
             adicionar();
@@ -20,9 +26,17 @@ while(continuar === true) {
             console.log(` Sobremesa: ${sobremesa}\n Café da Tarde: ${cafeDaTarde}\n Petisco: ${petisco}\n Misto: ${misto}\n Bebida: ${bebida}`);
             continuar = false;
             break;
+        case 'r':
+            if (podeRemover === false){
+                console.log('Você deve adicionar um item primeiro antes de remover!!');
+            } else {
+            categoriaRemover = prompt('Qual a categoria do item que você deseja remover ? ');
+            remover();
+            break;
+            }
     }
 }
-//         Adicionando os itens para suas devidas listas
+//         Adicionando os itens para suas devidas listas"
 function adicionar() {
     item = prompt('Digite o item que você deseja adicionar: ');
     categoria = prompt('Em que categoria está este item ? Sobremesa, café da tarde, petisco, misto ou bebida ? ');
@@ -53,4 +67,38 @@ function adicionar() {
 }
 function adicionarLista(lista) {
     lista.push(item);
+    podeRemover = true;
+}
+
+function remover() {
+    switch(categoriaRemover) {
+        case 'sobremesa':
+            removerLista(sobremesa);
+            console.log(`Sobremesa: ${sobremesa}`);
+            break;
+        case 'cafe':
+            removerLista(cafeDaTarde);
+            console.log(`Café da Tarde: ${cafeDaTarde}`);
+            break;
+        case 'petisco':
+            removerLista(petisco);
+            console.log(`Petisco: ${petisco}`);
+            break;
+        case 'misto':
+            removerLista(misto);
+            console.log(`Misto: ${misto} `);
+            break;
+        case 'bebida':
+            removerLista(bebida);
+            console.log(`Bebida ${bebida}`);
+            break;
+        default:
+            console.log('Categoria não encontrada!! Digite uma categoria válida: ');
+    }
+}
+
+function removerLista(lista) {
+    itemRemover = prompt('Qual item que você deseja remover ? ');
+    indexItem = lista.indexOf(itemRemover);
+    lista.splice(indexItem, 1);
 }
